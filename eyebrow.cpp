5e5571c710d2738050739be2273b5a4fac8219f9
Samyak Datta <datta.samyak@gmail.com>
2014-12-12 12:21:54 +0530

    Added 1st version of eyebrow detection program

---
+++ eyebrow.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: Samyak Datta (datta[dot]samyak[at]gmail.com)
+ *
+ * A program to automatically detect eyebrows and eyebrow key points using a color-based
+ * method of eyebrow segmentation that extracts a pseudo-hue plane to separate the eyebrow 
+ * from the skin region. 
+ *
+ * This code is an implementation of the following paper
+ * Majumder, A. Singh, M. and Behera, L., "Automatic eyebrow features detection and realization 
+ * of avatar for real time eyebrow movement", 7th IEEE International Conference on 
+ * Industrial and Information Systems (ICIIS),2012
+ */
+
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}

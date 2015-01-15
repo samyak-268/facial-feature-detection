86ff37e6ea5171c34a8837faf819954fd18f97ac
Samyak Datta <datta.samyak@gmail.com>
2015-01-15 17:24:10 +0530

    Initial commit for the mouth detection module

---
+++ mouth/mouth.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: Samyak Datta (datta[dot]samyak[at]gmail.com)
+ *
+ *
+ */
+
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+
+    return 0;
+}

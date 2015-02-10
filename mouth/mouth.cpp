d39c15df38f92a5f2ff7c0f10a55630d850b1b63
Samyak Datta <datta.samyak@gmail.com>
2015-02-10 15:03:16 +0530

    Color transformations for mouth detection

--- mouth/mouth.cpp
+++ mouth/mouth.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <iostream>
+#include <cmath>
 #include <cfloat>
 #include <climits>
 
@@ -15,8 +16,12 @@
 using namespace std;
 using namespace cv;
 
+Mat_<Vec3b> equalizeImage(Mat_<Vec3b> image_BGR);
 Mat_<uchar> transformPseudoHue(Mat_<Vec3b> image);
 pair<double, double> Stats(Mat_<double> pseudo_hue_plane);
+Mat_<uchar> transformCIELAB(Mat_<Vec3b> image_BGR);
+Mat_<uchar> transformLUX(Mat_<Vec3b> image_BGR);
+Mat_<uchar> transformModifiedLUX(Mat_<Vec3b> image_BGR);
 
 int main(int argc, char** argv)
 {
@@ -28,28 +33,30 @@ int main(int argc, char** argv)
     
     const string input_image_path = argv[1];
     Mat_<Vec3b> image_BGR = imread(input_image_path);
+    Mat_<uchar> image_BGR_eq = equalizeImage(image_BGR);
     
-    /* CIELAB transformation and using the A-channel
+    Mat_<uchar> pseudo_hue_plane = transformPseudoHue(image_BGR);
+    // Mat_<uchar> chrominance_plane = transformLUX(image_BGR);
+    Mat_<uchar> modified_chrominance_plane = transformModifiedLUX(image_BGR);
 
-    Mat_<Vec3b> image_Lab;
+    // imshow("Original-Image", image_BGR);
+    imshow("Pseudo-Hue", pseudo_hue_plane);
+    // imshow("Chrominance-Plane", chrominance_plane);
+    imshow("Modified-Chrominance-Plane", modified_chrominance_plane);
+
+    waitKey(0);
+    return 0;
+}
+
+/*
+ * Equalize a BGR image by converting it to the YCrCb space and
+ * performing histogram equalization on the Y-plane before
+ * merging back
+ */
+Mat_<Vec3b> equalizeImage(Mat_<Vec3b> image_BGR)
+{
     vector<Mat> channels;
-    cvtColor(image_BGR, image_Lab, CV_BGR2Lab);
-    split(image_Lab, channels);
-
-    Mat_<uchar> image_a = channels[1];
-
-    imshow("Original-Image", image_BGR);
-    imshow("A-Channel", image_a);
-    
-    */
-    
-    /* Convert image to YCrCb, equalize the Y-plane and merge back.
-     * Then apply the CIELAB transform to the equalized image and use the 
-     * A-channel for lip segmentation.
-     */
-    
-    vector<Mat> channels;
-    Mat image_eq;
+    Mat_<Vec3b> image_eq;
 
     cvtColor(image_BGR, image_eq, CV_BGR2YCrCb);
     split(image_eq, channels);
@@ -57,28 +64,10 @@ int main(int argc, char** argv)
     merge(channels, image_eq);
     cvtColor(image_eq, image_eq, CV_YCrCb2BGR);
 
-
-    Mat_<Vec3b> image_Lab;
-    vector<Mat> channels_1;
-    cvtColor(image_eq, image_Lab, CV_BGR2Lab);
-    split(image_Lab, channels_1);
-
-    Mat_<uchar> image_a = channels_1[1];
-
-    imshow("Original Image", image_BGR);
-    imshow("Equalized Image", image_eq);
-    imshow("A-channel", image_a);
-    // imshow("Pseudo-Hue", pseudo_hue_plane);
-
-    /*
-     * Use the pseudo-hue plane transformation to segment lip region.
-     */
-    Mat_<uchar> pseudo_hue_plane = transformPseudoHue(image_BGR);
-
-    waitKey(0);
-    return 0;
+    return image_eq;
 }
 
+// Extract the pseudo-hue plane
 Mat_<uchar> transformPseudoHue(Mat_<Vec3b> image)
 {
     Mat_<double> pseudo_hue(image.size());
@@ -132,4 +121,73 @@ pair<double, double> Stats(Mat_<double> pseudo_hue_plane)
         }
     }
     return make_pair(Hmin, Hmax);
+}
+
+// CIELAB transformation and using the A-channel
+Mat_<uchar> transformCIELAB(Mat_<Vec3b> image_BGR)
+{
+    Mat_<Vec3b> image_Lab;
+    vector<Mat> channels;
+    cvtColor(image_BGR, image_Lab, CV_BGR2Lab);
+    split(image_Lab, channels);
+
+    Mat_<uchar> image_a = channels[1];
+    return image_a;
+}
+
+Mat_<uchar> transformLUX(Mat_<Vec3b> image_BGR)
+{
+    Mat_<uchar> U(image_BGR.size());
+    
+    int B = 0, G = 0, R = 0, L_int = 0, u_int = 0;
+    double L = 0.0, u = 0.0;
+    for(int i = 0; i < image_BGR.rows; ++i)
+    {
+        for(int j = 0; j < image_BGR.cols; ++j)
+        {
+            B = image_BGR(i, j)[0];
+            G = image_BGR(i, j)[1];
+            R = image_BGR(i, j)[2];
+
+            L = (pow(R+1, 0.3) * pow(G+1, 0.6) * pow(B+1, 0.1)) - 1;
+            L_int = round(L);
+            
+            if(R > L_int)
+            {
+                u = (256 * (L_int+1)) / (R + 1);
+                u_int = round(u);
+                U.at<uchar>(i, j) = u_int;
+            }
+            else
+                U.at<uchar>(i, j) = 255;
+        }
+    }
+    return U;
+}
+
+Mat_<uchar> transformModifiedLUX(Mat_<Vec3b> image_BGR)
+{
+    Mat_<uchar> Ucap(image_BGR.size());
+    
+    int B = 0, G = 0, R = 0, u_cap_int = 0;
+    double u_cap = 0.0;
+    for(int i = 0; i < image_BGR.rows; ++i)
+    {
+        for(int j = 0; j < image_BGR.cols; ++j)
+        {
+            B = image_BGR(i, j)[0];
+            G = image_BGR(i, j)[1];
+            R = image_BGR(i, j)[2];
+
+            if(R > G)
+            {
+                u_cap = (256*G) / R;
+                u_cap_int = round(u_cap);
+                Ucap.at<uchar>(i, j) = u_cap_int;
+            }
+            else
+                Ucap.at<uchar>(i, j) = 255;
+        }
+    }
+    return Ucap;
 }

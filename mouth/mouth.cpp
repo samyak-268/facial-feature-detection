d4ada8232d9450c491efbde8bb61419f78606068
Samyak Datta <datta.samyak@gmail.com>
2015-02-12 22:35:42 +0530

    Added code for lip contour detection.

--- mouth/mouth.cpp
+++ mouth/mouth.cpp
@@ -29,6 +29,7 @@ Mat_<uchar> transformModifiedLUX(Mat_<Vec3b> image_BGR);
 
 pair<double, double> returnImageStats(const Mat_<uchar>& image);
 Mat_<uchar> binaryThresholding(const Mat_<uchar>& image, const pair<double, double>& stats);
+int returnLargestContourIndex(vector<vector<Point> > contours);
 
 int main(int argc, char** argv)
 {
@@ -48,12 +49,35 @@ int main(int argc, char** argv)
     Mat_<uchar> pseudo_hue_bin = binaryThresholding(pseudo_hue_plane, 
             returnImageStats(pseudo_hue_plane));
     
+    // A clone image is required because findContours() modifies the input image
+    Mat binary_clone = pseudo_hue_bin.clone();
+    vector<vector<Point> > contours;
+    findContours(binary_clone, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+    
+    // Initialize blank image (for drawing contours)
+    Mat_<uchar> image_contour(pseudo_hue_bin.size());
+    for(int i = 0; i < image_contour.rows; ++i)
+    {
+        for(int j = 0; j < image_contour.cols; ++j)
+            image_contour.at<uchar>(i, j) = 0;
+    }
+
+    // Draw largest contour on the blank image
+    cout << "Size of the contour image: " << image_contour.rows << " X " << image_contour.cols << "\n";
+    int largest_contour_idx = returnLargestContourIndex(contours);
+    for(int i = 0; i < contours[largest_contour_idx].size(); ++i)
+    {
+        Point_<int> pt = contours[largest_contour_idx][i];
+        image_contour.at<uchar>(pt.y, pt.x) = 255;
+    }
+    
     
     // imshow("Input-Image", image_BGR);
     // imshow("Face-ROI", face);
     // imshow("Mouth-ROI", mouth);
     imshow("Pseudo-Hue", pseudo_hue_plane);
-    imshow("Pseudo-Hue-Binary", pseudo_hue_bin);
+    // imshow("Pseudo-Hue-Binary", pseudo_hue_bin);
+    imshow("Contour", image_contour);
     
     /*
      * Mat_<uchar> chrominance_plane = transformLUX(image_BGR);
@@ -294,4 +318,19 @@ Mat_<uchar> binaryThresholding(const Mat_<uchar>& image, const pair<double, doub
         }
     }
     return image_binary;
+}
+
+int returnLargestContourIndex(vector<vector<Point> > contours)
+{
+    int max_contour_size = 0;
+    int max_contour_idx = -1;
+    for(int i = 0; i < contours.size(); ++i)
+    {
+        if(contours[i].size() > max_contour_size)
+        {
+            max_contour_size = contours[i].size();
+            max_contour_idx = i;
+        }
+    }
+    return max_contour_idx;
 }
